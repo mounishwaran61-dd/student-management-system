9df72f614177886a5bb935473620e02ceb24dc6c
Mounishwaran <mounishwaran61@gmail.com>
2026-09-13 16:27:44 +0530

    Fix Vercel allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g0*ga3p42^(y2=%ts*vv*1=oa0vt3&md#!--(hb8#0jimlq7*!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 # Application definition
