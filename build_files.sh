3718330c5be53b88b452a6caf03fe5f0008ae368
Mounishwaran <mounishwaran61@gmail.com>
2026-09-14 12:53:13 +0530

    Fix PEP 668 externally managed environment error in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+export PIP_BREAK_SYSTEM_PACKAGES=1
+
+if command -v python3 &>/dev/null; then
+    PYTHON_CMD=python3
+else
+    PYTHON_CMD=python
+fi
+
+$PYTHON_CMD -m pip install --break-system-packages -r requirements.txt
+$PYTHON_CMD manage.py collectstatic --noinput --clear
